3aebe39455cc30399c01b9c858143234fdb8f2e4
Jim Pick <jim@backblend.com>
2018-11-11 12:57:26 -0800

    Use ESM and async/await to query user stream

--- src/read-log.js
+++ src/read-log.js
@@ -1,11 +1,29 @@
-var ssbClient = require('ssb-client')
+import ssbClient from 'ssb-client'
+import pull from 'pull-stream'
+import { promisify } from 'util'
 
+const openSsbClient = promisify(ssbClient)
 
-ssbClient(function (err, sbot) {
-  if (err)
-    throw err
+async function run () {
+  try {
+    const sbot = await openSsbClient()
+    console.log('Jim ready', sbot)
+    const whoamiAsync = promisify(sbot.whoami)
+    const whoami = await whoamiAsync()
+    console.log('Jim whoami', whoami)
+    pull(
+      sbot.createUserStream({id: whoami.id}),
+      pull.collect((err, msgs) => {
+        if (err) throw err
+        msgs.forEach(msg => {
+          console.log(JSON.stringify(msg, null, 2))
+        })
+      })
+    )
+    sbot.close()
+  } catch (err) {
+    console.error('Exception', err)
+  }
+}
 
-	console.log('Jim ready', sbot)
-  // sbot is now ready. when done:
-  sbot.close()
-})
+run()
