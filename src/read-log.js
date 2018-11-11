e7c662b513d6aae67fcb170403b5ad0c658e5f1b
Jim Pick <jim@backblend.com>
2018-11-11 13:23:25 -0800

    Try async iterator out

--- src/read-log.js
+++ src/read-log.js
@@ -1,5 +1,6 @@
 import ssbClient from 'ssb-client'
 import pull from 'pull-stream'
+import toIterator from 'pull-stream-to-async-iterator'
 import { promisify } from 'util'
 
 const openSsbClient = promisify(ssbClient)
@@ -11,15 +12,19 @@ async function run () {
     const whoamiAsync = promisify(sbot.whoami)
     const whoami = await whoamiAsync()
     console.log('Jim whoami', whoami)
-    pull(
+    const source = pull(
       sbot.createUserStream({id: whoami.id}),
-      pull.collect((err, msgs) => {
-        if (err) throw err
-        msgs.forEach(msg => {
-          console.log(JSON.stringify(msg, null, 2))
-        })
+      pull.asyncMap((value, cb) => {
+        console.log('Jim sleep 1s')
+        setTimeout(() => {
+          cb(null, value)
+        }, 1000)
       })
     )
+    const iterator = toIterator(source)
+    for await (const value of iterator) {
+      console.log(JSON.stringify(value, null, 2))
+    }
     sbot.close()
   } catch (err) {
     console.error('Exception', err)
